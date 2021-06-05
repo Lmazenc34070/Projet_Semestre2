73b5ee48a54ab7affe4de88ce4a731102b50f9fb
MAZENC Lucas <72392592+Lmazenc34070@users.noreply.github.com>
2021-06-05 23:28:13 +0200

    Clean

--- src/scenes/TableauTiled.js
+++ src/scenes/TableauTiled.js
@@ -1,22 +1,10 @@
 class TableauTiled extends Tableau{
-    /**
-     * Ce tableau démontre comment se servir de Tiled, un petit logiciel qui permet de designer des levels et de les importer dans Phaser (entre autre).
-     *
-     * Ce qui suit est très fortement inspiré de ce tuto :
-     * https://stackabuse.com/phaser-3-and-tiled-building-a-platformer/
-     *
-     * Je vous conseille aussi ce tuto qui propose quelques alternatives (la manière dont son découpées certaines maisons notamment) :
-     * https://medium.com/@michaelwesthadley/modular-game-worlds-in-phaser-3-tilemaps-1-958fc7e6bbd6
-     */
     preload() {
         super.preload();
-        // ------pour TILED-------------
-        // nos images
         this.load.image('tiles', 'assets/Tiledmap/TileSheet2.png');
-        //les données du tableau qu'on a créé dans TILED
         this.load.tilemapTiledJSON('map', 'assets/Tiledmap/LevelTest2V10.json');
 
-        // -----et puis aussi-------------
+        // -----Preload-------------
         this.load.image('monster-fly', 'assets/RobotVole.png');
         this.load.image('PlateformMouv', 'assets/Plat_Sci_Fi1.png');
         this.load.image('Move', 'assets/Plat_Sci_Fi1.png');
@@ -33,30 +21,36 @@ class TableauTiled extends Tableau{
         this.load.image('fog', 'assets/Brume1.png')
         this.load.image('background', 'assets/Back_Sci_fi4.png');
         this.load.image('pixel', 'assets/pixel.png');
-
+        this.load.audio('ambiance','assets/Sounds/E_Ressurection.mp3');
     }
     create() {
         super.create();
 
-        //on en aura besoin...
         let ici=this;
 
-        //--------chargement de la tile map & configuration de la scène-----------------------
+        this.musicamb = this.sound.add('ambiance');
+        var musicConfig =
+            {
+                mute: false,
+                volume: 0.2,
+                rate : 1,
+                detune: 0,
+                seek: 0,
+                loop: true,
+                delay:0,
+            }
+        this.musicamb.play(musicConfig);
 
         //notre map
         this.map = this.make.tilemap({ key: 'map' });
         //nos images qui vont avec la map
         this.tileset = this.map.addTilesetImage('TileSheet2', 'tiles');
         
-
-        //on agrandit le champ de la caméra du coup
         let largeurDuTableau=this.map.widthInPixels;
         let hauteurDuTableau=this.map.heightInPixels;
         this.physics.world.setBounds(0, 0, largeurDuTableau,  hauteurDuTableau);
         this.cameras.main.setBounds(0, 0, largeurDuTableau, hauteurDuTableau);
         this.cameras.main.startFollow(this.player, true, 1, 1);
-
-        //---- ajoute les plateformes simples ----------------------------
 
         this.devant = this.map.createLayer('Devant', this.tileset, 0, 0);
         this.decor = this.map.createLayer('decor', this.tileset, 0, 0);
@@ -64,15 +58,10 @@ class TableauTiled extends Tableau{
 
         this.obstacle=this.map.createLayer('Obstacle', this/this.tileset, 0, 0);
 
-
-        //on définit les collisions, plusieurs méthodes existent:
-
         this.devant.setCollisionByProperty({ collides: true });
 
-        // 2 manière la plus simple (là où il y a des tiles ça collide et sinon non)
         this.devant.setCollisionByExclusion(-1, true);
         this.obstacle.setCollisionByExclusion(-1, true);
-
 
         this.Platforms = this.physics.add.group({
             allowGravity: false,
@@ -82,14 +71,10 @@ class TableauTiled extends Tableau{
         });
 
         this.PlatformsObjects = this.map.getObjectLayer('PlatMouv')['objects'];
-        // On crée des étoiles pour chaque objet rencontré
         this.PlatformsObjects.forEach(PlatformsObject => {
-            // Pour chaque étoile on la positionne pour que ça colle bien car les étoiles ne font pas 64x64
             let Platforms = this.Platforms.create(PlatformsObject.x+32, PlatformsObject.y+16 /*, 'particles'*/, 'PlateformMouv');
         });
-        //this.physics.add.collider(this.Platforms, this.player);
 
-        //this.physics.add.collider(this.Platforms, this.player);
         this.physics.add.collider(this.Platforms, this.player, function () {
         });
 
@@ -104,7 +89,6 @@ class TableauTiled extends Tableau{
 
 
         ici.robotMonsterObjects = ici.map.getObjectLayer('robotVole')['objects'];
-        // On crée des montres volants pour chaque objet rencontré
         ici.robotMonsterObjects.forEach(monsterObject => {
             let monster=new Speed(this,monsterObject.x,monsterObject.y);
             this.monstersContainer.add(monster);
@@ -113,14 +97,12 @@ class TableauTiled extends Tableau{
         });
 
         ici.robotMonsterObjects = ici.map.getObjectLayer('robotTir')['objects'];
-        // On crée des montres volants pour chaque objet rencontré
         ici.robotMonsterObjects.forEach(monsterObject => {
             let monster=new RobotTir(this,monsterObject.x,monsterObject.y);
             this.monstersContainer.add(monster);
             this.physics.add.collider(monster, this.devant);
         });
         ici.robotMonsterObjects = ici.map.getObjectLayer('TestEnnemi')['objects'];
-        // On crée des montres volants pour chaque objet rencontré
         ici.robotMonsterObjects.forEach(monsterObject => {
             let monster=new EnnemiTombe(this,monsterObject.x,monsterObject.y);
             this.monstersContainer.add(monster);
@@ -136,7 +118,6 @@ class TableauTiled extends Tableau{
         });
 
         ici.robotSuiveurObjects = ici.map.getObjectLayer('robotSuiveur')['objects'];
-        // On crée des montres volants pour chaque objet rencontré
         ici.robotSuiveurObjects.forEach(monsterObject => {
             let monster=new RobotSuiveur(this,monsterObject.x,monsterObject.y);
             this.monstersContainer.add(monster);
@@ -158,18 +139,14 @@ class TableauTiled extends Tableau{
                 laser.setEnable();
             }
             if(laserObject.properties[0].value === 4){
-                // laser.setTint(0xFFDD33);
                 laser.setAngle(90);
                 laser.setDisplaySize(30, 130);
                 laser.setBodySize(laser.body.width+75,laser.body.height-140);
-                // laser.setEnable();
             }
             if(laserObject.properties[0].value === 5){
-                // laser.setTint(0xFFDD33);
                 laser.setAngle(90);
                 laser.setDisplaySize(30, 215);
                 laser.setBodySize(laser.body.width+75,laser.body.height-140);
-                // laser.setEnable();
             }
             if(laserObject.properties[0].value === 6){
                 laser.setDisplaySize(30, 215);
@@ -177,14 +154,11 @@ class TableauTiled extends Tableau{
                 laser.setBodySize(laser.body.width+130,laser.body.height-140);
             }
             if(laserObject.properties[0].value === 7){
-                // laser.setTint(0xFFDD33);
                 laser.setAngle(90);
                 laser.setBodySize(laser.body.width+130,laser.body.height-140);
-                // laser.setEnable();
             }
             if(laserObject.properties[0].value === 8){
                 laser.setTint(0xFFDD33);
-                // laser.setEnable();
             }
             this.laserContainer.add(laser);
         });
@@ -207,9 +181,7 @@ class TableauTiled extends Tableau{
         ici.rebondObjects.forEach(rebondObject => {
             let rebond=new PlatRebond(this,rebondObject.x,rebondObject.y);
             this.rebondContainer.add(rebond);
-            // if(this.player.body.velocity.y > 0||this.player.getBounds().bottom < rebond.getBounds().top+30){
                 ici.physics.add.collider(this.player, rebond,this.Bounding,null,this);
-            // }    
         });
 
         ici.plight = ici.map.getObjectLayer('Lights')['objects'];
@@ -310,12 +282,7 @@ class TableauTiled extends Tableau{
         // // this.sky.setScrollFactor(1);
         // this.sky2.setScrollFactor(0);
 
-        //----------collisions---------------------
-
-        //quoi collide avec quoi?
         this.physics.add.collider(this.player, this.devant);
-
-        //--------- Z order -----------------------
 
         let z=1000; //niveau Z qui a chaque fois est décrémenté.
         debug.setDepth(z--);
@@ -333,12 +300,10 @@ class TableauTiled extends Tableau{
         this.decorArr.setDepth(z--);
         // this.sky2.setDepth(z--);
         // this.sky.setDepth(z--);
-
     }
 
     optimizeDisplay(){
         let world=this.cameras.main.worldView;
-
     }
 
     moveParallax(){
@@ -348,7 +313,6 @@ class TableauTiled extends Tableau{
         // this.sky2.tilePositionX = this.cameras.main.scrollX * 0.15;
         // this.sky2.tilePositionY = this.cameras.main.scrollY * 0.05;
     }
-
 
     update(){
         super.update();
@@ -366,12 +330,4 @@ class TableauTiled extends Tableau{
             this.optimizeDisplay();
         }
     }
-
-    
-
-    
-
-
-
-
 }
